4f007172cab50fea3c9c0e01afecc4a481c34c9d
srlee1990 <sarah_4029@outlook.com>
2024-09-15 05:25:03 +0900

    1

--- mergePngToPdf.js
+++ mergePngToPdf.js
@@ -4,7 +4,7 @@ const { PDFDocument } = require('pdf-lib');
 const { PNG } = require('pngjs');
 
 // Path to the root directory containing PNG files
-const imagesDir = 'C:/AAA/brochures/images/book/05-innk,teslab/tESLAB';
+const imagesDir = 'C:/AAA/brochures/images/book/06-CRO';
 
 // Function to recursively get all PNG files from a directory and its subdirectories
 function getPngFiles(dir) {
